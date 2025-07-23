b492e7761242d43be9ea81e8a9cc04a0115baeb8
Pheasa <113016697+Pheasa-Uon@users.noreply.github.com>
2025-07-24 01:52:12 +0700

    Reapply "fii"
    This reverts commit 971332c313f3ddab7f370d3bec9f8ab9c5205284.

---
+++ src/main/java/com/istar/service/Security/UserDetailsImpl.java
@@ -0,0 +1,55 @@
+package com.istar.service.Security;
+
+import com.istar.service.Entity.Administrator.UsersManagment.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // You can return roles/permissions here
+        return Collections.emptyList(); // if no roles
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
--- src/main/java/com/istar/service/dto/Administrator/UsersManagement/FeaturePermissionDTO.java
+++ src/main/java/com/istar/service/dto/Administrator/UsersManagement/FeaturePermissionDTO.java
@@ -5,8 +5,6 @@ public class FeaturePermissionDTO {
     private Long roleId;
     private Long featureId;
 
-    private String code;
-
     private Boolean isSearch;
     private Boolean isAdd;
     private Boolean isViewed;
@@ -20,154 +18,81 @@ public class FeaturePermissionDTO {
     private Boolean isProcess;
     private Boolean isImport;
     private Boolean isExport;
+
     private Boolean bStatus;
 
-    public FeaturePermissionDTO() {
-    }
+    // Constructors
+    public FeaturePermissionDTO() {}
 
-    public FeaturePermissionDTO(String code, Boolean isSearch, Boolean isAdd,
-                                Boolean isViewed, Boolean isEdit, Boolean isDeleted) {
-        this.code = code;
-        this.isSearch = isSearch;
-        this.isAdd = isAdd;
-        this.isViewed = isViewed;
-        this.isEdit = isEdit;
-        this.isDeleted = isDeleted;
-    }
-
-    public Long getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Long roleId) {
+    public FeaturePermissionDTO(Long roleId, Long featureId,
+                                Boolean isSearch, Boolean isAdd, Boolean isViewed, Boolean isEdit,
+                                Boolean isApprove, Boolean isReject, Boolean isDeleted, Boolean isSave,
+                                Boolean isClear, Boolean isCancel, Boolean isProcess,
+                                Boolean isImport, Boolean isExport, Boolean bStatus) {
         this.roleId = roleId;
-    }
-
-    public Long getFeatureId() {
-        return featureId;
-    }
-
-    public void setFeatureId(Long featureId) {
         this.featureId = featureId;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public Boolean getIsSearch() {
-        return isSearch;
-    }
-
-    public void setIsSearch(Boolean isSearch) {
         this.isSearch = isSearch;
-    }
-
-    public Boolean getIsAdd() {
-        return isAdd;
-    }
-
-    public void setIsAdd(Boolean isAdd) {
         this.isAdd = isAdd;
-    }
-
-    public Boolean getIsViewed() {
-        return isViewed;
-    }
-
-    public void setIsViewed(Boolean isViewed) {
         this.isViewed = isViewed;
-    }
-
-    public Boolean getIsEdit() {
-        return isEdit;
-    }
-
-    public void setIsEdit(Boolean isEdit) {
         this.isEdit = isEdit;
-    }
-
-    public Boolean getIsApprove() {
-        return isApprove;
-    }
-
-    public void setIsApprove(Boolean isApprove) {
         this.isApprove = isApprove;
-    }
-
-    public Boolean getIsReject() {
-        return isReject;
-    }
-
-    public void setIsReject(Boolean isReject) {
         this.isReject = isReject;
-    }
-
-    public Boolean getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(Boolean isDeleted) {
         this.isDeleted = isDeleted;
-    }
-
-    public Boolean getIsSave() {
-        return isSave;
-    }
-
-    public void setIsSave(Boolean isSave) {
         this.isSave = isSave;
-    }
-
-    public Boolean getIsClear() {
-        return isClear;
-    }
-
-    public void setIsClear(Boolean isClear) {
         this.isClear = isClear;
-    }
-
-    public Boolean getIsCancel() {
-        return isCancel;
-    }
-
-    public void setIsCancel(Boolean isCancel) {
         this.isCancel = isCancel;
-    }
-
-    public Boolean getIsProcess() {
-        return isProcess;
-    }
-
-    public void setIsProcess(Boolean isProcess) {
         this.isProcess = isProcess;
-    }
-
-    public Boolean getIsImport() {
-        return isImport;
-    }
-
-    public void setIsImport(Boolean isImport) {
         this.isImport = isImport;
-    }
-
-    public Boolean getIsExport() {
-        return isExport;
-    }
-
-    public void setIsExport(Boolean isExport) {
         this.isExport = isExport;
-    }
-
-    public Boolean getBStatus() {
-        return bStatus;
-    }
-
-    public void setBStatus(Boolean bStatus) {
         this.bStatus = bStatus;
     }
+
+    // Getters and Setters
+    public Long getRoleId() { return roleId; }
+    public void setRoleId(Long roleId) { this.roleId = roleId; }
+
+    public Long getFeatureId() { return featureId; }
+    public void setFeatureId(Long featureId) { this.featureId = featureId; }
+
+    public Boolean getIsSearch() { return isSearch; }
+    public void setIsSearch(Boolean isSearch) { this.isSearch = isSearch; }
+
+    public Boolean getIsAdd() { return isAdd; }
+    public void setIsAdd(Boolean isAdd) { this.isAdd = isAdd; }
+
+    public Boolean getIsViewed() { return isViewed; }
+    public void setIsViewed(Boolean isViewed) { this.isViewed = isViewed; }
+
+    public Boolean getIsEdit() { return isEdit; }
+    public void setIsEdit(Boolean isEdit) { this.isEdit = isEdit; }
+
+    public Boolean getIsApprove() { return isApprove; }
+    public void setIsApprove(Boolean isApprove) { this.isApprove = isApprove; }
+
+    public Boolean getIsReject() { return isReject; }
+    public void setIsReject(Boolean isReject) { this.isReject = isReject; }
+
+    public Boolean getIsDeleted() { return isDeleted; }
+    public void setIsDeleted(Boolean isDeleted) { this.isDeleted = isDeleted; }
+
+    public Boolean getIsSave() { return isSave; }
+    public void setIsSave(Boolean isSave) { this.isSave = isSave; }
+
+    public Boolean getIsClear() { return isClear; }
+    public void setIsClear(Boolean isClear) { this.isClear = isClear; }
+
+    public Boolean getIsCancel() { return isCancel; }
+    public void setIsCancel(Boolean isCancel) { this.isCancel = isCancel; }
+
+    public Boolean getIsProcess() { return isProcess; }
+    public void setIsProcess(Boolean isProcess) { this.isProcess = isProcess; }
+
+    public Boolean getIsImport() { return isImport; }
+    public void setIsImport(Boolean isImport) { this.isImport = isImport; }
+
+    public Boolean getIsExport() { return isExport; }
+    public void setIsExport(Boolean isExport) { this.isExport = isExport; }
+
+    public Boolean getBStatus() { return bStatus; }
+    public void setBStatus(Boolean bStatus) { this.bStatus = bStatus; }
 }
