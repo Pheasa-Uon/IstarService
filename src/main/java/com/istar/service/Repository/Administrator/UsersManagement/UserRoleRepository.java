3c97a54ce9de4ea99ef34615ea96596d25769eb3
Pheasa <113016697+Pheasa-Uon@users.noreply.github.com>
2025-07-29 13:31:55 +0700

    final fix set user permission

--- src/main/java/com/istar/service/Repository/Administrator/UsersManagement/UserRoleRepository.java
+++ src/main/java/com/istar/service/Repository/Administrator/UsersManagement/UserRoleRepository.java
@@ -1,5 +1,7 @@
 package com.istar.service.Repository.Administrator.UsersManagement;
 
+import com.istar.service.Entity.Administrator.UsersManagment.Role;
+import com.istar.service.Entity.Administrator.UsersManagment.User;
 import com.istar.service.Entity.Administrator.UsersManagment.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -13,7 +15,12 @@ import java.util.Optional;
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     List<UserRole> findByUserId(Long userId);
-    Optional<UserRole> findByUserIdAndRoleId(Long userId, Long roleId);
+    //Optional<UserRole> findByUserIdAndRoleId(Long userId, Long roleId);
+
+    @Query("SELECT ur FROM UserRole ur WHERE ur.user.id = :userId AND ur.role.id = :roleId")
+    Optional<UserRole> findByUserIdAndRoleId(@Param("userId") Long userId, @Param("roleId") Long roleId);
+
+    Optional<UserRole> findByUserAndRole(User user, Role role);
     boolean existsByUserIdAndRoleId(Long userId, Long roleId);
 
     @Query("SELECT ur FROM UserRole ur WHERE ur.user.id = :userId AND ur.bStatus = true")
